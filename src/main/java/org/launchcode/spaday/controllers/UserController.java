547fbde1e3edfcc6dc97d15fcf81f6faf3ccb930
kelseyjungermann <kelsey.jungemrann@gmail.com>
2023-05-18 20:14:30 -0500

    finishing studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,15 +1,36 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
+    @GetMapping("/add")
     public String displayAddUserForm() {
         return "user/add";
     }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
+        model.addAttribute("verify", verify);
+        model.addAttribute("user", user);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (!user.getPassword().equals(verify)) {
+            model.addAttribute("Error", "Passwords do not match.");
+            return "user/add";
+        } else {
+            return "user/index";
+        }
+
+    }
+
 }
